e64981ac364799478cdcabb0c150efbcb44b434f
diegoadlqa <diegoan.garcia@avaldigitallabs.com>
2026-07-10 15:51:54 -0500

    añadir archivos plantilla

---
+++ jira-config.example.js
@@ -0,0 +1,9 @@
+module.exports = {
+  JIRA_URL: "PON_AQUI_TU_URL_DE_JIRA",
+  ISSUE_KEY: "PON_AQUI_TU_ISSUE_KEY",
+  EMAIL: "PON_AQUI_TU_EMAIL",
+  API_TOKEN: "PON_AQUI_TU_TOKEN",
+  FOLDER_PATH: "PON_AQUI_TU_RUTA_LOCAL"
+};
+
+//https://id.atlassian.com/manage-profile/security/api-tokens
